02a3ed35dbb0fbdfbe1c027fbf9d599cb533aef0
Shailey Joseph <shaileyjoseph@Shaileys-MacBook-Air.local>
2023-04-15 11:54:34 -0700

    Initial commit

---
+++ ???.py
@@ -0,0 +1,16 @@
+import hmm
+
+subject = input("What subject do you want multiple choice questions on?\n")
+json_data = hmm.get_questions(subject)
+
+for question in json_data["questions"]:
+    print()
+    print(question["question"])
+    for option in question["options"]:
+        print(option)
+    guess = input()
+    if guess.upper() == question["answer"].upper():
+        print("correct!", question["explanation"])
+    else:
+        print("incorrect. The correct answer was", question["answer"], question["explanation"])
+
